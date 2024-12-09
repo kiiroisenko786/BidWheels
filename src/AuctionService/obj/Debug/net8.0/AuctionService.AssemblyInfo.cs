25d991cd6745d53a1c3dadfb1fb269fd85c1fc81
Taahaa <projectyuni5@gmail.com>
2024-12-09 09:41:52 +0000

    fixed db issue

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ff77db9cba44fe9fe9df91b594884a909cac5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33516d3271dc8b1ca96bdc228214b570003cc670")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
