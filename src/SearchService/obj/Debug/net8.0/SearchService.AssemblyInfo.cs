abf352303f188cde0ce0e65d0fb9ceea8466709d
kiiroisenko786 <projectyuni5@gmail.com>
2025-02-18 01:12:27 +0000

    add message retries

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd03c8528f250377dcc7f19b065be4244ac68b47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8179a86a89309221a7e14d6754db3af9b346c612")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
