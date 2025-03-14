ee87f7642fa64c9fb9ba0adfd5471455a9cced09
kiiroisenko786 <projectyuni5@gmail.com>
2025-03-14 02:18:38 +0000

    dockerized auction svc

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eec69682d074192f131c323bc48e1b26e239d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e4679eb931435bde836bbe76938a95ce89f2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eec69682d074192f131c323bc48e1b26e239d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e4679eb931435bde836bbe76938a95ce89f2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
