019e6cab422845eccd522f324a3f72cb539638f0
kiiroisenko786 <projectyuni5@gmail.com>
2025-03-19 03:29:06 +0000

    dockerizing and testing svc complete

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32bcab2e571cfc5b27ff0cb95042961a62c0e75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154f55630e9f0bfe06663f2ffd2bd68f4728fe82")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
