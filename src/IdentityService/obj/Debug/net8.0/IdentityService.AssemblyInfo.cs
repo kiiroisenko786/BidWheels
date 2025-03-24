0278194fadc97444035b37dfb7499c014b16eec4
kiiroisenko786 <projectyuni5@gmail.com>
2025-03-24 00:54:24 +0000

    fix tailwind versioning issue for flowbite

--- src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net8.0/IdentityService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03cb4d0baf213cf6691ea7aae551c6f9493befb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beae66572f7c8db48d2de810eba22a57a1174432")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
